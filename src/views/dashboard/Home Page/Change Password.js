bd9a929b26e6881957da699fce8827124e72b8ed
Gayath1 <chandulagayan@gmail.com>
2021-05-17 18:27:53 +0530

    changes

--- src/views/dashboard/Home Page/Change Password.js
+++ src/views/dashboard/Home Page/Change Password.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   CButton,
   CCard,
@@ -19,19 +19,49 @@ CTooltip,
 } from '@coreui/react'
 import { DocsLink } from 'src/reusable'
 import CIcon from '@coreui/icons-react'
+import axios from "axios";
 
 
-const changepassword = () => {
+const Changepassword = () => {
   const placements = [
     'top-start', 'top', 'top-end',
     'bottom-start', 'bottom', 'bottom-end',
     'right-start', 'right', 'right-end',
     'left-start', 'left', 'left-end'
   ]
+  const orgid = localStorage.getItem("org")
+  const token = localStorage.getItem("Token")
+  const id = localStorage.getItem("id")
+  const [password, setPassword] = useState();
+  const onChangePassword = (e) => {
+    setPassword(e.target.value );
+  };
+  const onSubmit = async (data) => {
 
+
+    const body = ({password,id} );
+    axios.defaults.baseURL = "https://hrm-innovigent.herokuapp.com/api/v1";
+
+    const headers = {
+      headers: {
+
+        "Authorization":`Bearer ${token}`
+      }
+    };
+
+    axios.post(`/organizations/${orgid}/employeeslist/updatepassword`, body, headers)
+      .then((res) => {
+        if (res.status === 200) {
+          alert('Change success');
+        }
+      }).catch((err) => {
+      console.error(err);
+      alert('Error please try again');
+    });
+  };
   return (
     <>
-      
+
 
       <hr/>
 
@@ -40,7 +70,7 @@ const changepassword = () => {
         <b> CHANGE PASSWORD</b>
         </CCardHeader>
         <CCardBody>
-           <CForm action="" method="post">
+           <CForm >
            <CFormGroup>
                   <CInputGroup>
                     <CInputGroupPrepend>
@@ -49,13 +79,13 @@ const changepassword = () => {
                     <CInput type="password" id="password1" name="password1" placeholder="type old Password" autoComplete="current-password"/>
                   </CInputGroup>
                 </CFormGroup>
-               
+
                 <CFormGroup>
                   <CInputGroup>
                     <CInputGroupPrepend>
                       <CInputGroupText><CIcon name="cil-asterisk" /></CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="password" id="password1" name="password1" placeholder="enter new Password" autoComplete="current-password"/>
+                    <CInput type="password" id="password1" name="password1" placeholder="enter new Password" onChange={onChangePassword}  autoComplete="current-password"/>
                   </CInputGroup>
                 </CFormGroup>
 
@@ -68,7 +98,7 @@ const changepassword = () => {
                   </CInputGroup>
                 </CFormGroup>
                 <CFormGroup className="form-actions">
-                  <CButton type="submit" size="lg" color="success">Submit</CButton>
+                  <CButton  size="lg" color="success" onClick={onSubmit}>Submit</CButton>
                 </CFormGroup>
               </CForm>
         </CCardBody>
@@ -77,4 +107,4 @@ const changepassword = () => {
   )
 }
 
-export default changepassword;
+export default Changepassword;
