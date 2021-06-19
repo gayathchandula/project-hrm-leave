6aa5e8ec98753135aab0619e51ebccdbd11df4dd
Gayath1 <chandulagayan@gmail.com>
2021-06-19 10:40:25 +0530

    employee overtime code quality changes

--- src/views/dashboard/Overtime Management/Employee Overtime.js
+++ src/views/dashboard/Overtime Management/Employee Overtime.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useContext} from 'react';
+import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 
 
@@ -9,32 +9,11 @@ import {
   CCardHeader,
   CCol,
   CDataTable,
-  CButton,
-  CForm,
-  CSelect,
-  CTabs,
-  CTabContent,
-  CNavItem,
-  CNavLink,
-  CNav,
-  CTabPane,
-  CFormGroup,
-  CFormText,
-  CCardFooter,
-  CInput,
-  CLabel,
   CRow,
 } from '@coreui/react'
-import { DocsLink } from 'src/reusable'
-import usersData from '../../users/UsersData'
-import UserContext from '../../../userContext';
 
-import { Calendar, momentLocalizer} from 'react-big-calendar'
+
 import moment from 'moment';
-import {
-  useHistory
-} from "react-router-dom";
-
 
 
 
@@ -61,53 +40,12 @@ const fields = ['employeeId','OTHrs', 'OTDayRate', 'createdAt','reviewstatusId']
 
 
 const Tables = () => {
-  const [otHrsMax, setotHrsMax] = useState([]);
-  const [otHrsMin, setotHrsMin] = useState([]);
-  const [incharge_email, setincharge_email] = useState([]);
-  const [employeeTypeId, setemployeeTypeId] = useState([]);
-  const [otRate, setotRate] = useState([]);
-  const [otAllowancePercentage, setotpercentage] = useState([]);
-  const [SpecialDate, setdate] = useState([]);
-  const [SpecialDayName, setspecial] = useState([]);
-  const { userData, setUserData } = useContext(UserContext);
-  const [HolidayInfo, setHolidayInfo] = useState([]);
+
   const [listData, setListData] = useState({ lists: [] });
-  const [listData1, setListData1] = useState({ lists: [] });
-  const [listData2, setListData2] = useState({ lists: [] });
-  const [listData3, setListData3] = useState({ lists: [] });
   const token = localStorage.getItem("Token")
   const empId = localStorage.getItem("id")
-  const history = useHistory();
-  const localizer = momentLocalizer(moment)
-  const onChangeMax_Hours = (e) => {
-    setotHrsMax(e.target.value);
-  };
-  const onChangedate = (e) => {
-    setdate(e.target.value);
-  };
-  const onChangeMin_Hours = (e) => {
-    setotHrsMin(e.target.value );
-  };
-  const onChangeemploy_type = (e) => {
-    setemployeeTypeId( e.target.value );
-  };
-  const onChangeInchemail = (e) => {
-    setincharge_email(e.target.value );
-  };
-  const onChangehour_rate = (e) => {
-    setotRate( e.target.value );
-  };
-  const onChangepercentage = (e) => {
 
-    setotpercentage( e.target.value );
-  };
-  const onChangespecialday = (e) => {
-    setspecial( e.target.value );
-  };
 
-  const onChangeHolidayInfo = (e) => {
-    setHolidayInfo( e.target.value );
-  };
   const headers = {
     headers: {
 
@@ -128,16 +66,6 @@ const Tables = () => {
   }, []);
 
 
-
-  const events= listData3.lists.map((appointment)=>{
-    return {
-      id: appointment.id,
-      title: appointment.SpecialName,
-      start: new Date(appointment.SpecialDate),
-      end: new Date(appointment.SpecialDate),
-      allDay: true
-    }
-  })
   return (
 
     <>
@@ -159,9 +87,9 @@ const Tables = () => {
                 size="sm"
                 itemsPerPage={10}
                 pagination
-                scopedSlots = {{
+                scopedSlots={{
                   'status':
-                    (item)=>(
+                    (item) => (
                       <td>
                         <CBadge color={getBadge(item.status)}>
                           {item.status}
@@ -169,7 +97,7 @@ const Tables = () => {
                       </td>
                     ),
                   'reviewstatusId':
-                    (item)=>(
+                    (item) => (
                       <td>
                         <CBadge color={getBadge(item.reviewstatusId)}>
                           {changestatus(item.reviewstatusId)}
